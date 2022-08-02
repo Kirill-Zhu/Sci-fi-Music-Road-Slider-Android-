87dbde31a388f0f743a62f0f318420abb86ac890
KirillZhu <k.zhuravlev1992@yandex.ru>
2022-08-02 19:19:46 +0300

    Added Main Menu and Start menu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,66 @@
-
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 public class GameManager : MonoBehaviour
-{
+{    
+    public static GameManager insntance;
 
-
+    [SerializeField] AudioMixerGroup masterMixer;
+    
+    [Tooltip("Attach the main menu Game Object")]
+    [SerializeField] GameObject mainMenu;
+    private bool _mainMenuIsOpened = false;
+    private void Start()
+    {
+        #region SINGLETONE
+        if (insntance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+            insntance = this;
+        #endregion
+        GlobalEventSystem.OnPlayPauseGame += Open_CloseMainMenu;
+    }
+    private void OnDestroy()
+    {
+        GlobalEventSystem.OnPlayPauseGame -= Open_CloseMainMenu;
+    }
     private void Update()
     {
-       if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            GlobalEventSystem.SendOnPlayPauseGame();
-        }           
+            Open_CloseMainMenu();
+        }
     }
 
     public void ExitGame()
     {
-     #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-     #else
+#else
         Application.Quit();
-     #endif
+#endif
+    }
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
     }
 
-   
+    public void ChangeMasterVolume(float volume)
+    {
+        if (masterMixer != null)
+            masterMixer.audioMixer.SetFloat("MasterVolume", volume);
+    }
+    public void Open_CloseMainMenu()
+    {if(mainMenu!=null)
+        {
+            _mainMenuIsOpened = !_mainMenuIsOpened;
+            if (_mainMenuIsOpened)
+                mainMenu.SetActive(true);
+            else
+                mainMenu.SetActive(false);
+        }
+
+    }
 }
